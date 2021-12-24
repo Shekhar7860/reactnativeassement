da3f6ed91f60d969cda36a4acbf4f5ee83937b10
unknown <shekhar.chugh.henceforth@gmail.com>
2021-12-24 13:47:42 +0530

    'codecommited'

--- actionTypes.js
+++
@@ -1,8 +0,0 @@
-import * as actionTypes from './actionTypes';
-
-export const createContact = (contact) => {
-    return {
-      type: actionTypes.CREATE_NEW_CONTACT,
-      contact: contact
-    }
-  };
